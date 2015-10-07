95e6ac315d58ee854909405be23d0e76606238ef
toyners <toyners@hotmail.com>
2015-10-08 10:39:04 +1100

    Changed Nuget package push from powershell command to vanilla Nuget push command. Powershell script and command still in project

--- Jabberwocky.Toolkit/_CreateNewNuGetPackage/RunMeToUploadNuGetPackage.cmd
+++ Jabberwocky.Toolkit/_CreateNewNuGetPackage/RunMeToUploadNuGetPackage.cmd
@@ -5,5 +5,9 @@ REM When you run this script it will prompt you for a NuGet package file (.nupkg
 REM The project's .nupkg file should be in the same directory as the project's .dll/.exe file (typically bin\Debug or bin\Release).
 REM You may edit the Config.ps1 file to adjust the settings used to upload the package to the NuGet gallery.
 REM To run this script from within Visual Studio, right-click on this file from the Solution Explorer and choose Run.
-SET THIS_SCRIPTS_DIRECTORY=%~dp0
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%THIS_SCRIPTS_DIRECTORY%DoNotModify\UploadNuGetPackage.ps1'"
+REM SET THIS_SCRIPTS_DIRECTORY=%~dp0
+REM PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%THIS_SCRIPTS_DIRECTORY%DoNotModify\UploadNuGetPackage.ps1'"
+
+C:\C#\Jabberwocky.Toolkit\Jabberwocky.Toolkit\_CreateNewNuGetPackage\DoNotModify\NuGet.exe push C:\C#\Jabberwocky.Toolkit\Jabberwocky.Toolkit\bin\release\*.nupkg -Source C:\NugetRepository -NonInteractive
+
+pause
