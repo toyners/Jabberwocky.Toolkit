180db49974b0ead4a8e3615d8dd195f276290106
toyners <toyners@hotmail.com>
2015-10-21 21:58:35 +1100

    Added StringOperations class with ExtractField extension method and unit tests.

---
+++ Jabberwocky.Toolkit/String/StringOperations.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Jabberwocky.Toolkit.String
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public static class StringOperations
+  {
+    #region Methods
+    public static String ExtractField(this String instance, String seperator, Char qualifier, UInt32 fieldIndex)
+    {
+      Int32 position = 0;
+      Int32 index = 0;
+      while (true)
+      {
+        Int32 seperateIndex = 0;
+
+        Boolean isQualified = false;
+        for (; index < instance.Length; index++)
+        {
+          if (instance[index] == qualifier)
+          {
+            isQualified = !isQualified;
+          }
+
+          if (!isQualified && instance[index] == seperator[seperateIndex])
+          {
+            break;
+          }
+        }
+
+        if (index == -1)
+        {
+          return instance.Substring(position, instance.Length - position);
+        }
+
+        seperateIndex++;
+        Boolean gotSeperator = true;
+        for (; seperateIndex < seperator.Length; seperateIndex++)
+        {
+          index++;
+          if (index == instance.Length)
+          {
+            // End of line. Seperator was not completed.
+            break;
+          }
+
+          if (instance[index] != seperator[seperateIndex])
+          {
+            gotSeperator = false;
+            break;
+          }
+        }
+
+        if (!gotSeperator)
+        {
+          continue;
+        }
+
+        if (fieldIndex == 1)
+        {
+          position = index;
+        }
+        else if (fieldIndex == 0)
+        {
+          //Int32 count = index - position;
+          if (instance[position] == qualifier)
+          {
+            position++;
+          }
+
+          if (index > 0 && instance[index - 1] == qualifier)
+          {
+            index--;
+          }
+
+          return instance.Substring(position, index - position);
+        }
+
+        fieldIndex--;
+      }
+
+      throw new NotImplementedException();
+    }
+    #endregion
+  }
+}
---
+++ Jabberwocky.Toolkit_UnitTests/String/StringOperations_UnitTests.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Jabberwocky.Tookit_UnitTests.String
+{
+  using System;
+  using FluentAssertions;
+  using NUnit.Framework;
+  using Jabberwocky.Toolkit.String;
+
+  [TestFixture]
+  public class StringOperations_UnitTests
+  {
+    #region Methods
+    [Test]
+    public void ExtractField_SeperatorNotInLine_WholeLineReturned()
+    {
+      String line = "ABCD";
+
+      line.ExtractField(",", '\0', 0).Should().Be("ABCD");
+    }
+
+    [Test]
+    public void ExtractField_IndexBeyondLine_ThrowsMeaningfulException()
+    {
+      String line = "A,B,C,D";
+
+      Action action = () => line.ExtractField(",", '\0', 4);
+      
+      action.ShouldThrow<IndexOutOfRangeException>().WithMessage("Index 4 is out of range in line 'A,B,C,D' when using seperator (\",\") and qualifier('\0').");
+    }
+
+    [Test]
+    [TestCase(0, "A,B,C,D", "A")]
+    [TestCase(1, "A,B,C,D", "B")]
+    [TestCase(2, "A,B,C,D", "C")]
+    [TestCase(3, "A,B,C,D", "D")]
+    [TestCase(0, "|A,1|B,C,D", "A,1")]
+    public void ExtractField_AskForField_ReturnsField(Int32 index, String line, String expectedResult)
+    {
+      line.ExtractField(",", '|', (UInt32)index).Should().Be(expectedResult);
+    }
+    #endregion
+  }
+}
