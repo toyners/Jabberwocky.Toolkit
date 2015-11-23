015e54f0931e460d965c6ffed9240041e1b96ea1
toyners <toyners@hotmail.com>
2015-11-23 11:36:13 +1100

    Added static class that contains path like functionality. Added first method that ensures a directory path string will have a seperator character on the end. Added unit tests for this functionality. Upgraded to 1.0.2.2

---
+++ Jabberwocky.Toolkit/Path/PathOperations.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Jabberwocky.Toolkit.Path
+{
+  using System;
+  using System.IO;
+  using Validation;
+
+  /// <summary>
+  /// Provides methods implementing path like functionality.
+  /// </summary>
+  public static class PathOperations
+  {
+    #region Methods
+    /// <summary>
+    /// Will complete a directory path by appending a seperator character (\) is necessary.
+    /// </summary>
+    /// <param name="directoryPath">Directory path to append the seperator character to.</param>
+    /// <returns>Directory path with seperator character appended.</returns>
+    public static String CompleteDirectoryPath(String directoryPath)
+    {
+      directoryPath.VerifyThatStringIsNotNullAndNotEmpty("Parameter 'directoryPath' is null or empty.");
+      if (directoryPath[directoryPath.Length - 1] != Path.DirectorySeparatorChar)
+      {
+        directoryPath += Path.DirectorySeparatorChar;
+      }
+
+      return directoryPath;
+    }
+    #endregion
+  }
+}
---
+++ Jabberwocky.Toolkit_FUnitTests/Path Operations UnitTests.fs
@@ -0,0 +1,16 @@
+﻿namespace Jabberwocky.Toolkit_FUnitTests
+
+open Jabberwocky.Toolkit.Path
+open FsUnit
+open NUnit.Framework
+
+module ``Path Operations UnitTests`` =
+
+    [<Test>]
+    let ``Directory path has no trailing seperator``() =
+        PathOperations.CompleteDirectoryPath(@"C:\Directory") |> should equal @"C:\Directory\"
+
+    [<Test>]
+    let ``Directory path has trailing seperator``() =
+        PathOperations.CompleteDirectoryPath(@"C:\Directory\") |> should equal @"C:\Directory\"
+
