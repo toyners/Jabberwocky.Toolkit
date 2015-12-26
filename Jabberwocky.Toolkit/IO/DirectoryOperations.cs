5e086ee633e490d49257d4de630140081b8a8342
toyners <toyners@hotmail.com>
2015-12-26 15:27:17 +1300

    Added DirectoryOperations class with EnsureDirectoryExists method. Added unit tests. Updated to 1.0.2.6.

---
+++ Jabberwocky.Toolkit/IO/DirectoryOperations.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Jabberwocky.Toolkit.IO
+{
+  using System;
+  using System.IO;
+  using Jabberwocky.Toolkit.Validation;
+
+  /// <summary>
+  /// Helper methods for directory functionality. 
+  /// </summary>
+  public static class DirectoryOperations
+  {
+    /// <summary>
+    /// Checks that the directory exists. If not then it creates the directory.
+    /// </summary>
+    /// <param name="directoryPath">Full path to the directory to check.</param>
+    public static void EnsureDirectoryExists(String directoryPath)
+    {
+      directoryPath.VerifyThatStringIsNotNullAndNotEmpty("Parameter 'directoryPath' is null or empty.");
+      if (!Directory.Exists(directoryPath))
+      {
+        Directory.CreateDirectory(directoryPath);
+      }
+    }
+  }
+}
---
+++ Jabberwocky.Toolkit_FUnitTests/DirectoryOperations Unit Tests.fs
@@ -0,0 +1,27 @@
+﻿namespace Jabberwocky.Toolkit_FUnitTests
+
+open System.IO
+open FsUnit
+open Jabberwocky.Toolkit.IO
+open NUnit.Framework
+
+module ``DirectoryOperations Unit Tests`` =
+
+    [<Test>]
+    let ``Directory does not exist so is created``() =
+        let directoryPath = Path.GetTempPath() + Path.GetRandomFileName();
+
+        Directory.Exists(directoryPath) |> should be False
+        DirectoryOperations.EnsureDirectoryExists(directoryPath) |> ignore
+        Directory.Exists(directoryPath) |> should be True
+
+        Directory.Delete(directoryPath) |> ignore
+
+    [<Test>]
+    let ``Directory does exist so nothing changes``() =
+        let directoryPath = Path.GetTempPath() + Path.GetRandomFileName();
+        Directory.CreateDirectory(directoryPath) |> ignore
+        DirectoryOperations.EnsureDirectoryExists(directoryPath) |> ignore
+        Directory.Exists(directoryPath) |> should be True
+
+        Directory.Delete(directoryPath) |> ignore
