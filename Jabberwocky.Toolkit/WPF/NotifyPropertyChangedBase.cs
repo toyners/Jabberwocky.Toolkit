f2151a3817e07ede58fbe5a25a5080fab23d819d
Benjamin Toynbee <toyners@hotmail.com>
2017-01-12 21:00:09 +1100

    Added NotifyPropertyChangedBase class with tests

---
+++ Jabberwocky.Toolkit/WPF/NotifyPropertyChangedBase.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Jabberwocky.Toolkit.WPF
+{
+  using System;
+  using System.Collections.Generic;
+  using System.ComponentModel;
+  using System.Linq;
+  using System.Runtime.CompilerServices;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public class NotifyPropertyChangedBase : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void SetField<T>(ref T fieldValue, T newValue, [CallerMemberName] String propertyName = null)
+    {
+      if (EqualityComparer<T>.Default.Equals(fieldValue, newValue))
+      {
+        return;
+      }
+
+      fieldValue = newValue;
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+}
---
+++ Jabberwocky.Toolkit_IntegrationTests/WPF/NotifyPropertyChangedBase_IntegrationTests.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Jabberwocky.Toolkit_IntegrationTests.WPF
+{
+  using System;
+  using NUnit.Framework;
+  using Shouldly;
+  using Toolkit.WPF;
+
+  /// <summary>
+  /// Tests for the logic within the NotifyPropertyChangedBase class.
+  /// </summary>
+  [TestFixture]
+  public class NotifyPropertyChangedBase_IntegrationTests : NotifyPropertyChangedBase
+  {
+    private Boolean propertyChangedEventWasTriggered;
+
+    private Int32 value;
+
+    #region Methods
+    [SetUp]
+    public void SetupBeforeEachTest()
+    {
+      this.value = 0;
+      this.propertyChangedEventWasTriggered = false;
+      this.PropertyChanged -= this.PropertyChangedEventHandler;
+    }
+
+    /// <summary>
+    /// Test that the property change event is raised when the field is changed.
+    /// </summary>
+    [Test]
+    public void PropertyChangeEventHandlerTriggeredWhenFieldChanged()
+    {
+      // Arrange
+      this.PropertyChanged += this.PropertyChangedEventHandler;
+
+      // Act
+      this.SetField(ref value, 1);
+
+      // Assert
+      this.propertyChangedEventWasTriggered.ShouldBeTrue();
+    }
+
+    /// <summary>
+    /// Test that the property change event is not raised when the field is changed but
+    /// no event handler is assigned.
+    /// </summary>
+    [Test]
+    public void PropertyChangeEventHandlerNotTriggeredWhenFieldChanged()
+    {
+      // Act
+      this.SetField(ref value, 1);
+
+      // Assert
+      this.propertyChangedEventWasTriggered.ShouldBeFalse();
+    }
+
+    /// <summary>
+    /// Test that the property change event is not raised if the field is not changed
+    /// because the value is the same.
+    /// </summary>
+    [Test]
+    public void PropertyChangeEventHandlerNotTriggeredWhenFieldIsNotChanged()
+    {
+      // Arrange
+      this.SetField(ref value, 1);
+      this.PropertyChanged += PropertyChangedEventHandler;
+
+      // Act
+      this.SetField(ref value, 1);
+
+      // Assert
+      this.propertyChangedEventWasTriggered.ShouldBeFalse();
+    }
+
+    private void PropertyChangedEventHandler(Object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+      this.propertyChangedEventWasTriggered = true;
+    }
+    #endregion 
+  }
+}
