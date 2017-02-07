d634bdede59a5ae085effb610f54f45e1f21b74a
Benjamin Toynbee <toyners@hotmail.com>
2017-02-08 09:10:27 +1300

    Added interface for simple logging. Updated to 1.0.7.2

---
+++ Jabberwocky.Toolkit/Logging/ILogger.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Jabberwocky.Toolkit.Logging
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public interface ILogger
+  {
+    void Message(String message);
+
+    void Message(String message, Boolean lineBreak);
+
+    void Exception(String message);
+  }
+}
