c8d176cfa772d263909823ea875e7a68442e9226
Benjamin Toynbee <toyners@hotmail.com>
2017-02-08 12:04:53 +1300

    Added ILoggerFactory interface. Added comments. Updated to 1.0.7.3

--- Jabberwocky.Toolkit/Logging/ILogger.cs
+++ Jabberwocky.Toolkit/Logging/ILogger.cs
@@ -2,17 +2,29 @@
 namespace Jabberwocky.Toolkit.Logging
 {
   using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  using System.Text;
-  using System.Threading.Tasks;
 
+  /// <summary>
+  /// Provides functionality for logging general messages and exceptions.
+  /// </summary>
   public interface ILogger
   {
+    /// <summary>
+    /// Logs a general message.
+    /// </summary>
+    /// <param name="message">Message content to be logged.</param>
     void Message(String message);
 
+    /// <summary>
+    /// Logs a general message. Allows an optional line break to be appended to the message content.
+    /// </summary>
+    /// <param name="message">Message content to be logged.</param>
+    /// <param name="lineBreak">True to append line break, otherwise false.</param>
     void Message(String message, Boolean lineBreak);
 
+    /// <summary>
+    /// Logs an exception message.
+    /// </summary>
+    /// <param name="message">Message content to be logged.</param>
     void Exception(String message);
   }
 }
---
+++ Jabberwocky.Toolkit/Logging/ILoggerFactory.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Jabberwocky.Toolkit.Logging
+{
+  /// <summary>
+  /// Provides functionality to create instances of ILogger type.
+  /// </summary>
+  public interface ILoggerFactory
+  {
+    /// <summary>
+    /// Creates a new instance of the ILogger type.
+    /// </summary>
+    /// <returns>ILogger instance.</returns>
+    ILogger Create();
+  }
+}
