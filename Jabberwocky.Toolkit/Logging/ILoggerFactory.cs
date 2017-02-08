8c743e73a86224c0cb7f3da64fe8fab020f52e1a
Benjamin Toynbee <toyners@hotmail.com>
2017-02-08 19:01:56 +1300

    Added string parameter to ILoggerFactory create method. Updated to 1.0.7.4

--- Jabberwocky.Toolkit/Logging/ILoggerFactory.cs
+++ Jabberwocky.Toolkit/Logging/ILoggerFactory.cs
@@ -1,6 +1,8 @@
 ﻿
 namespace Jabberwocky.Toolkit.Logging
 {
+  using System;
+
   /// <summary>
   /// Provides functionality to create instances of ILogger type.
   /// </summary>
@@ -9,7 +11,8 @@ namespace Jabberwocky.Toolkit.Logging
     /// <summary>
     /// Creates a new instance of the ILogger type.
     /// </summary>
+    /// <param name="name">Name of ILogger instance.</param>
     /// <returns>ILogger instance.</returns>
-    ILogger Create();
+    ILogger Create(String name);
   }
 }
