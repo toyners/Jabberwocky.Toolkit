8e14b28d1ad55917a7d218e65419b2e6a75708e2
Benjamin Toynbee <toyners@hotmail.com>
2017-05-30 17:49:40 +1000

    Started adding generic path finding functionality

---
+++ Jabberwocky.Toolkit/PathFinding/PathFinder.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Jabberwocky.Toolkit.PathFinding
+{
+  using System;
+  using System.Collections.Generic;
+
+  public class PathFinderBase<T>
+  {
+    public List<T> GetPathBetweenPoints(T start, T end)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
